b26fbf45b0d838a23ff890aa95b8558aeffba1f7
Abizer-R <rabizeridris@gmail.com>
2022-06-23 11:21:58 +0530

    Added Bottom Navigation

---
+++ app/src/main/java/com/example/socialmediaapp_1/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.socialmediaapp_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.socialmediaapp_1.Fragments.AddFragment;
+import com.example.socialmediaapp_1.Fragments.HomeFragment;
+import com.example.socialmediaapp_1.Fragments.NotificationFragment;
+import com.example.socialmediaapp_1.Fragments.ProfileFragment;
+import com.example.socialmediaapp_1.Fragments.SearchFragment;
+import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Listener for Bottom Navigation View
+    private NavigationBarView.OnItemSelectedListener btmNavListener =
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_notification:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_add:
+                            selectedFragment = new AddFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_nav_fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation_view);
+        bottomNav.setOnItemSelectedListener(btmNavListener);
+    }
+}
