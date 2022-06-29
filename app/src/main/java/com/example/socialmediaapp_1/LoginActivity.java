88d2f783e27b690969f4b5ecbbf10934ca5b6757
Abizer-R <rabizeridris@gmail.com>
2022-06-29 16:33:42 +0530

    Implemented Login and register design

---
+++ app/src/main/java/com/example/socialmediaapp_1/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.socialmediaapp_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.socialmediaapp_1.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/socialmediaapp_1/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.socialmediaapp_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.socialmediaapp_1.databinding.ActivityRegisterBinding;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ActivityRegisterBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
